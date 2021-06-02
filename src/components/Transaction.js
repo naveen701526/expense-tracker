27c69fefddf995bfd722a9b61fc1102ecf936d6a
naveen <neonew8984@gmail.com>
2021-06-02 22:33:38 +0530

    expense tracker app

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export const Transaction = ({transaction}) => {
+    // Get sign
+    const sign = transaction.amount < 0 ? '-' : '+';
     return (
-        <li className='minus'>
-            {transaction.text} <span>-$400</span>
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+            {transaction.text}{' '}
+            <span>
+                {sign}
+                &#8377;{Math.abs(transaction.amount)}
+            </span>
             <button className='delete-btn'>x</button>
         </li>
     );
