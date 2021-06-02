31d90fe568d4cd7a40324fe147bd41aa54955445
naveen <neonew8984@gmail.com>
2021-06-02 21:38:56 +0530

    expense tracker app

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -4,7 +4,7 @@ export const TransactionList = () => {
     return (
         <>
             <h3>History</h3>
-            <ul id='list' class='list'>
+            <ul class='list'>
                 <li class='minus'>
                     Cash <span>-$400</span>
                     <button class='delete-btn'>x</button>
